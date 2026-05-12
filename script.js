70ddc980a128e69ddb4d2311ab2775ca6d6b3f01
noor2025-design <abdun221@newschool.edu>
2026-05-12 12:26:36 -0400

    added animation toggle states in js for figure backgrounds in motion

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-let figureBackgrounds = document.querySelectorAll(".figure-background")
+let figureElements = document.querySelectorAll(".motion figure");
+let figureBackgrounds = document.querySelectorAll(".figure-background");
 
-figureBackgrounds.forEach((element)=>{
-element.addEventListener("click", (event)=>{
-    event.currentTarget.classList.add("animate-out")
-})
-})
+figureElements.forEach((element) => {
+  element.addEventListener("click", (event) => {
+    let figureElement = event.currentTarget;
+    let figureBackground = figureElement.querySelector(".figure-background");
+    let isActive = figureBackground.classList.contains("animate")
+    figureBackgrounds.forEach((element) => element.classList.remove("animate"));
+    if (!isActive) {
+        figureBackground.classList.add("animate")
+    }
+  });
+});
